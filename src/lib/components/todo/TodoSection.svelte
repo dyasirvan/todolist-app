7d351f535f0205f5a6e5e41ab07f8732341211dc
dyasirvan <dyas.mail@gmail.com>
2023-05-06 15:09:38 +0700

    initial commit

---
+++ src/lib/components/todo/TodoSection.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {todos} from "$lib/store/TodoStore.js";
+    import TodoInput from "$lib/components/todo/TodoInput.svelte";
+</script>
+
+<section class="mt-8 space-y-4">
+    {#each $todos as todo (todo.id)}
+        <TodoInput {todo}/>
+    {/each}
+</section>
